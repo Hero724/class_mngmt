fdec16fbaeca4972cc113b8405003620252c8fa6
Danielle <mdlee724@gmail.com>
2014-02-19 19:27:26 -0800

    added scopes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,22 @@
 class Student < ActiveRecord::Base
+
+#has_many :courses
+has_many :registrations
+has_many :courses, through: :registrations
+
+scope :danielle_lee, ->  do
+where(full_name: 'danielle_lee')	
+end
+
+def self.teachers
+	where(title: 'Teacher')
+end
+
+def combined_info
+	"name: #{full_name} \n" +
+	"age: #{age} \n" +
+	"bio #{bio}"
+
+end
+
 end
